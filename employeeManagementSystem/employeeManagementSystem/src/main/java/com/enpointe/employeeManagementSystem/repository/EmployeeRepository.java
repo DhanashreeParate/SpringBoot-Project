f5ab1f67e057c4125acef850ec2a51a43764cd3d
DhanashreeParate <dhanashree.parate09@gmail.com>
2025-07-24 02:49:18 +0530

    Add files via upload

---
+++ employeeManagementSystem/employeeManagementSystem/src/main/java/com/enpointe/employeeManagementSystem/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.enpointe.employeeManagementSystem.repository;
+
+import com.enpointe.employeeManagementSystem.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ employeeManagementSystem/employeeManagementSystem/src/main/java/com/enpointe/employeeManagementSystem/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package com.enpointe.employeeManagementSystem.service;
+
+import com.enpointe.employeeManagementSystem.model.Employee;
+import com.enpointe.employeeManagementSystem.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll(); //batch operation
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee with id " + id + " not found in db."));
+    }
+
+    public Employee updateEmployeeById(Long id, Employee updateEmployee) {
+        Employee existingEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id: " + id));
+
+        existingEmployee.setName(updateEmployee.getName());
+        existingEmployee.setDepartment(updateEmployee.getDepartment());
+        existingEmployee.setSalary(updateEmployee.getSalary());
+
+        return employeeRepository.save(existingEmployee); // Save updated entity
+    }
+
+    public void deleteEmployee(Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Employee not found with id: " + id));
+        employeeRepository.delete(employee);
+    }
+}
